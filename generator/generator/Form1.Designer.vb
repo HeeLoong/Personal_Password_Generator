58e9c8095f69462b942db1c2320849cebdf3da0e
HeeLoong <128220537+HeeLoong@users.noreply.github.com>
2023-04-03 22:44:29 +0800

    Add files via upload

---
+++ generator/generator/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.result = New System.Windows.Forms.TextBox()
+        Me.wordbox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Location = New System.Drawing.Point(248, 260)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(262, 59)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(128, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(131, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Character of Password :"
+        '
+        'result
+        '
+        Me.result.Location = New System.Drawing.Point(129, 128)
+        Me.result.Multiline = True
+        Me.result.Name = "result"
+        Me.result.Size = New System.Drawing.Size(591, 112)
+        Me.result.TabIndex = 2
+        Me.result.Text = "Password Generate here"
+        '
+        'wordbox1
+        '
+        Me.wordbox1.Location = New System.Drawing.Point(265, 82)
+        Me.wordbox1.Name = "wordbox1"
+        Me.wordbox1.Size = New System.Drawing.Size(100, 23)
+        Me.wordbox1.TabIndex = 3
+        Me.wordbox1.Text = "8"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(826, 364)
+        Me.Controls.Add(Me.wordbox1)
+        Me.Controls.Add(Me.result)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Text = "GeneratorV1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents result As TextBox
+    Friend WithEvents wordbox1 As TextBox
+End Class
---
+++ generator/generator/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Function Rand(ByVal Low As Long, ByVal High As Long) As Long
+        Rand = Int((High - Low + 1) * Rnd()) + Low
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Const GEN = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
+        Dim intCounter As Integer
+        Dim strKey As String
+        Dim intIndex As Integer
+        strKey = ""
+        Randomize()
+
+        Dim sltin As Integer
+        sltin = wordbox1.Text
+
+        For intCounter = 1 To sltin
+            intIndex = Rand(1, Len(GEN))
+            strKey = strKey & Mid$(GEN, intIndex, 1)
+        Next
+        result.Text = strKey
+
+    End Sub
+
+End Class
